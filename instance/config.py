5090ff5d1edca846fda87433ebff989d9d18d6d9
sriaki <42790098+sriaki@users.noreply.github.com>
2019-04-14 22:02:31 -0400

    add config files

---
+++ instance/config.py
@@ -0,0 +1,33 @@
+# config.py
+
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///application.db'
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
